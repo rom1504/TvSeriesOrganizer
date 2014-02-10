e7b6ffb09c4cd7eed4ff38737c8de9b54778c0ef
Romain Beaumont <romain.rom1@gmail.com>
2014-02-10 13:19:41 +0100

    trying something different : downloading a Qt archive from my server

--- travis_script.sh
+++ travis_script.sh
@@ -9,7 +9,7 @@ export ANDROID_NDK_TOOLCHAIN_PREFIX=arm-linux-androideabi
 export ANDROID_SDK_ROOT=$CI_HOME/android-sdk-linux
 export ANDROID_HOME=$CI_HOME/android-sdk-linux
 export ANDROID_NDK_PLATFORM=android-18
-$CI_HOME/android_armv7/bin/qmake -r -spec $CI_HOME/android_armv7/mkspecs/android-g++
+/opt/Qt/5.2.1/android_armv7/bin/qmake -r -spec $CI_HOME/android_armv7/mkspecs/android-g++
 make
 make install INSTALL_ROOT=$CI_HOME/android-build/
-androiddeployqt --input ./TvSeriesOrganizerApp/android-libTvSeriesOrganizerApp.so-deployment-settings.json --output $CI_HOME/android-build/ --android-platform android-18 --release
+/opt/Qt/5.2.1/android_armv7/bin/androiddeployqt --input ./TvSeriesOrganizerApp/android-libTvSeriesOrganizerApp.so-deployment-settings.json --output $CI_HOME/android-build/ --android-platform android-18 --release
