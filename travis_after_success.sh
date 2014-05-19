4de8340c8b79b050a8143a70cfc2a9053df5fe03
Romain Beaumont <romain.rom1@gmail.com>
2014-05-19 21:29:02 +0200

    add irc notifications and fix tablet screenshot taking

--- travis_after_success.sh
+++ travis_after_success.sh
@@ -8,6 +8,6 @@ openssl aes-256-cbc -pass "pass:$MY_SECRET_ENV" -in .travis/id_rsa_deploy.enc -o
 chmod 600 ~/.ssh/id_rsa
 ./deploy.sh
 sudo gcc usleep.c -o /bin/usleep
-sudo apt-get install -qq xautomation x11-utils x11-apps
+sudo apt-get install -qq xautomation x11-utils x11-apps bc
 time ./multiScreenshot.sh 6 TvSeriesOrganizerApp/TvSeriesOrganizer /tmp/datadir_ screenshot/screenshot_ 
 ./sendScreenShot.sh
